436c4a6d5cb5895589b779ab5c45dfad3815e88d
Simon <349283970@qq.com>
2021-04-22 18:11:09 +0800

    Initial commit

---
+++ app/src/main/java/org/linphone/ui/message/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package org.linphone.ui.message
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/org/linphone/ui/mine/MineViewModel.kt
@@ -0,0 +1,12 @@
+package org.linphone.ui.mine
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MineViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
